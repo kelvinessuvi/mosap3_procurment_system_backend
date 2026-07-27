2ac2c6d2a6dac04ad07588b41ed36fc71f79303b
Kelvin Domingos <kelvindomingos20@hotmail.com>
2026-07-27 19:00:49 +0100

    fix: make activity_type field nullable for flexibility in supplier registration

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -154,7 +154,7 @@ class SupplierController extends Controller
             'alt_phone' => 'nullable|string|max:20',
             'nif' => 'required|string|unique:suppliers',
             // Update to match Swagger enums or accept both. Let's make it flexible.
-            'activity_type' => 'string', 
+            'activity_type' => 'nullable|string', 
             'province' => 'required|string',
             'municipality' => 'required|string',
             'address' => 'nullable|string',
